71225182c743624db63f2182748e3784a9b107b3
HJC <huchardjc@gmail.com>
2024-05-14 16:52:03 +0200

    ✨ MAIN > Create Pages, Router, Header, Footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
 import { Provider } from 'react-redux'
 import { store } from './store';
+import { BrowserRouter } from 'react-router-dom';
+import Router from '../components/Router';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function App() {
-  return <Provider store={store}>
-    <div className='App'></div>
+  return <BrowserRouter>
+    <Provider store={store}>
+      <div className='App'>
+        <Header />
+        <Router />
+        <Footer />
+      </div>
     </Provider>
+  </BrowserRouter>
 }
 
 export default App
---
+++ src/components/Router.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.js'
+import LoginPage from './pages/LoginPage.js'
+import UserPage from './pages/UserPage.js'
+
+export default function Router() {
+    return <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/user/:id' element={<UserPage />} />
+    </Routes>
+}
